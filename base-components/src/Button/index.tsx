a825e55c2ee7ecc8a892e828b6344ce7cfb1816e
zuoqi705 <1398772969@qq.com>
2020-10-08 21:45:55 +0800

    feat(Button): adjust button

--- base-components/src/Button/index.tsx
+++ base-components/src/Button/index.tsx
@@ -7,16 +7,15 @@ type NextButtonProps = React.ComponentProps<typeof NextButton>
 const rxTwoToFourCNChar = /^[\u4e00-\u9fa5]{2,4}$/
 const isTwoToFourCNChar = rxTwoToFourCNChar.test.bind(rxTwoToFourCNChar)
 
-const Button: React.FC<NextButtonProps> = props => {
-  const children = props.children
+const Button: React.FC<NextButtonProps> = ({ children, ...restProps }) => {
   if (typeof children === 'string' && isTwoToFourCNChar(children)) {
     return (
-      <NextButton className="isTwoToFourCNCharBtn" {...props}>
+      <NextButton className="isTwoToFourCNCharBtn" {...restProps}>
         {children}
       </NextButton>
     )
   }
-  return <NextButton {...props}>{children}</NextButton>
+  return <NextButton {...restProps}>{children}</NextButton>
 }
 
 hoistNonReactStatics(Button, NextButton)
