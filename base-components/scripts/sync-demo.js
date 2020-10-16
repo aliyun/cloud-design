b1d373175ad422d30d36847650dfbc9310f5cb88
sirui.csr <sirui.csr@alibaba-inc.com>
2020-10-16 11:43:29 +0800

    sync markdown

--- base-components/scripts/sync-demo.js
+++ base-components/scripts/sync-demo.js
@@ -1,13 +1,13 @@
 const glob = require('glob')
 const fs = require('fs-extra')
-const child = require('child_process')
 const path = require('path')
 const assert = require('assert')
 const prettier = require('prettier')
 
-// child.execSync('git clone')
-
+// 运行这个脚本之前，把这个参数改成本地fusion仓库的docs目录
 const fusionDocs = '/Users/csr/workspace/next/docs'
+
+
 const root = path.join(__dirname, '../')
 const prettierConfig = fs.readJsonSync(path.join(root, '../.prettierrc'))
 
@@ -15,28 +15,27 @@ const blocklist = ['pagination/demo/react-router.md']
 
 glob('*/demo/*.md', { cwd: fusionDocs }, (err, res) => {
   if (err) throw err
-
   res
     .filter((demoFile) => !blocklist.includes(demoFile))
     .forEach((demoFile) => {
-      if (demoFile.includes('react-router')) console.log(demoFile)
       const match = /^(.*?)\/demo\/(.*)\.md/.exec(demoFile)
       const componentName = match[1]
       const demoFilePath = match[2]
       const content = fs.readFileSync(path.join(fusionDocs, demoFile), 'utf8')
       const contentLines = content.split('\n')
-      // if (!/^\s*# (.*)/.exec(content)) debugger
+      // 获取demo标题
       const title = /^\s*# (.*)/.exec(content)[1]
+      // 获取demo描述
       let description = contentLines.find((line) => {
         return line && !line.startsWith('#') && !line.startsWith('-')
       })
       // 这个demo没有描述
       if (description.startsWith(':::')) description = ''
       assert(title)
-
-      // if (!/```jsx([\s\S]*?)```/.exec(content)) debugger
+      // 提取demo的代码块内容
       const demo = /```\s*jsx([\s\S]*?)```/.exec(content)[1]
       assert(demo)
+      // 解析ReactDOM.render(<JSX />, mountNode);语句
       const reactdom_render_regexp = /^ReactDOM\.render\(([\s\S]*),\s*mountNode\s*(?:,\s*)?\);?/m
       if (
         content.match(
@@ -46,13 +45,15 @@ glob('*/demo/*.md', { cwd: fusionDocs }, (err, res) => {
         throw new Error(`unexpected demo: ${demoFile}
       has multiple ReactDOM.render`)
       }
-      // if (!reactdom_render_regexp.exec(content)) debugger
+      // 提取JSX
       const renderJSX = reactdom_render_regexp.exec(content)[1]
       assert(renderJSX)
 
+      // 提取css
       const cssMatch = /```\s*css([\s\S]*?)```/.exec(content)
       const css = cssMatch ? cssMatch[1] : ''
 
+      // 产生的demo不应该直接调用ReactDOM.render，而是export一个DemoComponent
       let generateCode = demo.replace(reactdom_render_regexp, '')
       generateCode = generateCode.replace(
         /from ['"]@alifd\/next['"]/,
@@ -86,7 +87,7 @@ ${generateCode}`
         ...prettierConfig,
         parser: 'babel'
       })
-      assert(!/@alif\/next/.test(generateCode))
+      assert(!/@alifd\/next/.test(generateCode))
       const writePath = path.join(
         root,
         `src/${componentName}/demo/${demoFilePath}.tsx`
@@ -94,4 +95,4 @@ ${generateCode}`
       fs.ensureDirSync(path.dirname(writePath))
       fs.writeFileSync(writePath, generateCode)
     })
-})
+});
