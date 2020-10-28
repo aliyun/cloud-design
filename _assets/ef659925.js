f1bc0a9f3d89cc3a5372472d149d11d5e5eb2ae7
zuoqi705 <1398772969@qq.com>
2020-10-28 14:23:46 +0800

    Updates

---
+++ _assets/ef659925.js
@@ -0,0 +1,33 @@
+import {O as HOC, I as useState, a9 as useCallback, R as React, d as classnames} from "./index.26359282.js";
+import {N as NextSearch} from "./d3b47a40.js";
+const Search = (props) => {
+  const [focus, setFocus] = useState(false);
+  const [visible, setVisible] = useState(false);
+  const onFocus = useCallback((e) => {
+    setFocus(true);
+    if (typeof props.onFocus === "function") {
+      props.onFocus(e);
+    }
+  }, [props.onFocus]);
+  const onBlur = useCallback((e) => {
+    setFocus(false);
+    if (typeof props.onBlur === "function") {
+      props.onBlur(e);
+    }
+  }, [props.onBlur]);
+  const onVisibleChange = useCallback((v, ...rest) => {
+    setVisible(v);
+    if (typeof props.onVisibleChange === "function") {
+      props.onVisibleChange(v, ...rest);
+    }
+  }, [props.onVisibleChange]);
+  return /* @__PURE__ */ React.createElement(NextSearch, {
+    ...props,
+    onFocus,
+    onBlur,
+    onVisibleChange,
+    className: classnames(props.className, props.searchText ? "custom-search-text" : null, focus ? "focusing" : false, visible ? "visible" : false, props.disabled ? "disabled" : false)
+  });
+};
+var Search$1 = HOC(Search);
+export {Search$1 as S};
