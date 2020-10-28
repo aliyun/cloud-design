4d11f89644ecb42a864df9a7879f9590462a4a72
zuoqi705 <1398772969@qq.com>
2020-10-28 20:24:22 +0800

    feat: add aone-theme

--- base-components/scripts/update-theme.js
+++ base-components/scripts/update-theme.js
@@ -6,23 +6,25 @@ const themes = require('./theme-data');
 const cwd = process.cwd();
 const log = console.log;
 
-const getThemeSource = async(packageName, themeName) => {
+const getThemeSource = async(packageName, themeName, ignore) => {
   const unpkgUrl = 'https://unpkg.alibaba-inc.com/';
   const themePath = path.join(cwd, `fusion-theme/${themeName}`);
   log(chalk.green('empty files...'));
   fs.emptyDirSync(themePath);
-  try {
-    await download(`${unpkgUrl}${packageName}/dist/next.var.css`, themePath);
-    await download(`${unpkgUrl}${packageName}/dist/next-noreset.var.css`, themePath);
-    await download(`${unpkgUrl}${packageName}/variables.css`, themePath);
-    log(chalk.green(`update theme ${themeName} successfully`));
-  } catch(err) {
-    console.error(err);
+  if (!ignore) {
+    try {
+      await download(`${unpkgUrl}${packageName}/dist/next.var.css`, themePath);
+      await download(`${unpkgUrl}${packageName}/dist/next-noreset.var.css`, themePath);
+      await download(`${unpkgUrl}${packageName}/variables.css`, themePath);
+      log(chalk.green(`update theme ${themeName} successfully`));
+    } catch(err) {
+      console.error(err);
+    }
   }
 }
 
 themes.forEach(theme => {
-  const { packageName, themeName } = theme;
-  getThemeSource(packageName, themeName);
+  const { packageName, themeName, ignore } = theme;
+  getThemeSource(packageName, themeName, ignore);
 })
 
