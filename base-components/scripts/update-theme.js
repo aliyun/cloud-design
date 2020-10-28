8d53cb4da281c36e4e9c0672e66698267011f45f
zuoqi705 <1398772969@qq.com>
2020-10-28 20:26:55 +0800

    fix: fix update-theme

--- base-components/scripts/update-theme.js
+++ base-components/scripts/update-theme.js
@@ -6,18 +6,18 @@ const themes = require('./theme-data');
 const cwd = process.cwd();
 const log = console.log;
 
-const getThemeSource = async(packageName, themeName, ignore) => {
+const getThemeSource = async (packageName, themeName, ignore) => {
   const unpkgUrl = 'https://unpkg.alibaba-inc.com/';
-  const themePath = path.join(cwd, `fusion-theme/${themeName}`);
-  log(chalk.green('empty files...'));
-  fs.emptyDirSync(themePath);
   if (!ignore) {
+    const themePath = path.join(cwd, `fusion-theme/${themeName}`);
+    log(chalk.green('empty files...'));
+    fs.emptyDirSync(themePath);
     try {
       await download(`${unpkgUrl}${packageName}/dist/next.var.css`, themePath);
       await download(`${unpkgUrl}${packageName}/dist/next-noreset.var.css`, themePath);
       await download(`${unpkgUrl}${packageName}/variables.css`, themePath);
       log(chalk.green(`update theme ${themeName} successfully`));
-    } catch(err) {
+    } catch (err) {
       console.error(err);
     }
   }
