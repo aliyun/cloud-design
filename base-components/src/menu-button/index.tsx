b49f311906b31606c003c7182e331ea3345a5242
zuoqi705 <1398772969@qq.com>
2020-11-04 15:38:09 +0800

    fix: fix menubutton ts error

--- base-components/src/menu-button/index.tsx
+++ base-components/src/menu-button/index.tsx
@@ -1,7 +1,13 @@
+import * as React from 'react'
+import { MenuButton as NextMenuButton } from '@alifd/next'
+import HOC from '../utils/popupHoc'
 
-  import { MenuButton as NextMenuButton } from '@alifd/next'
-  import HOC from '../utils/popupHoc'
+type NextMenuButtonProps = React.ComponentProps<typeof NextMenuButton>
 
-  export default HOC(NextMenuButton);
-    
-  
+let MenuButton: typeof NextMenuButton = ((props: NextMenuButtonProps) => {
+  return <NextMenuButton autoWidth={false} {...props} />
+}) as any
+
+MenuButton = HOC(MenuButton) as any
+
+export default MenuButton
--- base-components/src/select/index.tsx
+++ base-components/src/select/index.tsx
@@ -1,6 +1,5 @@
 import { Select as NextSelect } from '@alifd/next'
 import React from 'react'
-import hoistNonReactStatics from 'hoist-non-react-statics'
 import HOC from '../utils/popupHoc'
 
 type NextSelectProps = React.ComponentProps<typeof NextSelect>
@@ -9,11 +8,9 @@ let Select: typeof NextSelect = React.forwardRef(
   (props: NextSelectProps, ref) => {
     return <NextSelect autoWidth={false} {...props} ref={ref as any} />
   }
-)as any
+) as any
 
 Select = HOC(Select) as any
-
-hoistNonReactStatics(Select, NextSelect)
 
 Select.AutoComplete = HOC(NextSelect.AutoComplete) as any
 
