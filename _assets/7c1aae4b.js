ff2f063a910f604e4bd845cb6badc71d42151a1f
zuoqi705 <1398772969@qq.com>
2020-11-08 15:50:25 +0800

    Updates

---
+++ _assets/7c1aae4b.js
@@ -0,0 +1,11 @@
+import {R as React} from "./index.112f9bd1.js";
+import {N as NextBalloon} from "./ef980e79.js";
+const {Tooltip: NextTooltip} = NextBalloon;
+const Tooltip = React.forwardRef((props, ref) => {
+  return /* @__PURE__ */ React.createElement(NextTooltip, {
+    delay: 200,
+    ...props,
+    ref
+  });
+});
+NextBalloon.Tooltip = Tooltip;
