c1dcc5281bfe2d8d9d8f3137bf88f71b26d59469
zuoqi705 <1398772969@qq.com>
2020-11-30 20:24:48 +0800

    style(Message): update message

--- base-components/src/message/demo/toast-quick.tsx
+++ base-components/src/message/demo/toast-quick.tsx
@@ -8,7 +8,7 @@ import styled from 'styled-components'
 
 import { Message, Button } from '@alicloudfe/components'
 
-const showSuccess = () => Message.success('success')
+const showSuccess = () => Message.success({ title: 'success', duration: 0})
 const showWarning = () => Message.warning('warning')
 const showError = () => Message.error('error')
 const showNotice = () => Message.notice('notice')
