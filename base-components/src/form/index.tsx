cd4616b08fc09dad58e9d468a58a568774007e31
zuoqi705 <1398772969@qq.com>
2020-12-24 16:46:12 +0800

    feat: update theme

--- base-components/src/form/index.tsx
+++ base-components/src/form/index.tsx
@@ -1,6 +1,18 @@
 
   import { Form as NextForm } from '@alifd/next'
+  // import React from 'react'
+  // import hoistNonReactStatics from 'hoist-non-react-statics'
 
+  // type NextFormProps = React.ComponentProps<typeof NextForm>
+
+  // const Form: typeof NextForm = React.forwardRef(
+  //   (props: NextFormProps, ref) => {
+  //     return <NextForm labelTextAlign="left" {...props} ref={ref as any} />
+  //   }
+  // )as any
+
+  // hoistNonReactStatics(Form, NextForm)
+  
   export default NextForm;
     
   
