de8257d5081530b6276f67d2c35342838362812b
sirui.csr <sirui.csr@alibaba-inc.com>
2020-12-24 17:00:29 +0800

    fix dialog footer align

--- base-components/src/dialog/index.tsx
+++ base-components/src/dialog/index.tsx
@@ -1,16 +1,18 @@
 import { Dialog as NextDialog } from '@alifd/next'
 import hoistNonReactStatics from 'hoist-non-react-statics'
 import React from 'react'
+import { useCssVar } from '../utils/useCssVar'
 
 type IProps = React.ComponentProps<typeof NextDialog>
 
 const Dialog: React.FC<IProps> = ({ ...props }) => {
-  return (
-    <NextDialog
-      footerAlign='left'
-      {...props}
-    />
-  )
+  const theme = useCssVar('--alicloudfe-components-theme').trim()
+  const defaultFooterAlign = (() => {
+    if (theme === 'wind' || theme === 'xconsole') return 'right'
+    return 'left'
+  })()
+
+  return <NextDialog footerAlign={defaultFooterAlign} {...props} />
 }
 
 hoistNonReactStatics(Dialog, NextDialog)
