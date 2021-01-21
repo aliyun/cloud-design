89525fea1a1e0f9a79626f6ce83ddff6bcbe0d6b
sirui.csr <sirui.csr@alibaba-inc.com>
2021-01-21 19:10:56 +0800

    fix wind button

--- base-components/src/button/index.tsx
+++ base-components/src/button/index.tsx
@@ -3,44 +3,54 @@ import React from 'react'
 import hoistNonReactStatics from 'hoist-non-react-statics'
 import cls from 'classnames'
 
+import { withThemeClass } from '../utils/withThemeClass'
+
 type NextButtonProps = React.ComponentProps<typeof NextButton>
 
 const rxTwoToFourCNChar = /^[\u4e00-\u9fa5]{2,4}$/
 const isTwoToFourCNChar = rxTwoToFourCNChar.test.bind(rxTwoToFourCNChar)
 
-const Button: typeof NextButton = React.forwardRef(
-  (props: NextButtonProps, ref) => {
+const Button: typeof NextButton = withThemeClass(
+  React.forwardRef((props: NextButtonProps, ref) => {
     const { children, className } = props
-     // 判断是否是2-4个汉字
-  if (typeof children === 'string' && isTwoToFourCNChar(children)) {
+    // 判断是否是2-4个汉字
+    if (typeof children === 'string' && isTwoToFourCNChar(children)) {
+      return (
+        <NextButton
+          {...props}
+          className={cls('isTwoToFourCNCharBtn', className)}
+          ref={ref as any}
+        >
+          {children}
+        </NextButton>
+      )
+    }
+    // 判断是否只有图标
+    if (
+      React.Children.count(children) === 1 &&
+      (children as any)?.type?.displayName === 'Config(Icon)'
+    ) {
+      return (
+        <NextButton
+          {...props}
+          className={cls('isOnlyIcon', className)}
+          ref={ref as any}
+        >
+          {children}
+        </NextButton>
+      )
+    }
     return (
-      <NextButton
-        {...props}
-        className={cls('isTwoToFourCNCharBtn', className)}
-        ref={ref as any}
-      >
+      <NextButton {...props} className={className} ref={ref as any}>
         {children}
       </NextButton>
     )
-  }
-  // 判断是否只有图标
-  if (
-    React.Children.count(children) === 1 &&
-    (children as any)?.type?.displayName === 'Config(Icon)'
-  ) {
-    return (
-      <NextButton {...props} className={cls('isOnlyIcon', className)} ref={ref as any}>
-        {children}
-      </NextButton>
-    )
-  }
-  return <NextButton {...props} className={className} ref={ref as any}>{children}</NextButton>
-  }
-)as any
+  })
+) as any
 
 hoistNonReactStatics(Button, NextButton)
 
-export default Button;
+export default Button
 
 // const exported: typeof NextButton = Button as any
 
--- base-components/src/utils/withThemeClass.tsx
+++ base-components/src/utils/withThemeClass.tsx
@@ -5,19 +5,22 @@ import { useCssVar } from './useCssVar'
 
 export function withThemeClass<T>(Comp: T) {
   // 使用useCssVar为它动态添加类名
-  const Wrapped: React.FC<{ className?: string }> = (props) => {
-    const theme = useCssVar('--alicloudfe-components-theme')
-    const isWind = theme.trim() === 'wind'
-    return (
-      // @ts-ignore
-      <Comp
-        {...props}
-        className={cls(props.className, {
-          ['is-wind']: isWind
-        })}
-      />
-    )
-  }
+  const Wrapped: React.FC<{ className?: string }> = React.forwardRef(
+    (props, ref) => {
+      const theme = useCssVar('--alicloudfe-components-theme')
+      const isWind = theme.trim() === 'wind'
+      return (
+        // @ts-ignore
+        <Comp
+          {...props}
+          ref={ref}
+          className={cls(props.className, {
+            ['is-wind']: isWind
+          })}
+        />
+      )
+    }
+  )
   hoistNonReactStatics(Wrapped, Comp as any)
   return (Wrapped as any) as T
 }
