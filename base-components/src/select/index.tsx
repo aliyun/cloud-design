d9929a4bd227d7828a40a6780a9663ff5df1901d
sirui.csr <sirui.csr@alibaba-inc.com>
2021-01-21 20:25:46 +0800

    fix wind select menu icon

--- base-components/src/select/index.tsx
+++ base-components/src/select/index.tsx
@@ -1,15 +1,33 @@
 import { Select as NextSelect } from '@alifd/next'
 import React from 'react'
 import hoistNonReactStatics from 'hoist-non-react-statics'
+
 import HOC from '../utils/popupHoc'
+import { useCssVar } from '../utils/useCssVar'
 
 type NextSelectProps = React.ComponentProps<typeof NextSelect>
 
 let Select: typeof NextSelect = React.forwardRef(
   (props: NextSelectProps, ref) => {
-    return <NextSelect autoWidth={false} {...props} ref={ref as any} />
+    const theme = useCssVar('--alicloudfe-components-theme')
+    const isWind = theme.trim() === 'wind'
+    const menuProps = (() => {
+      if (!isWind) return props.menuProps
+      return {
+        isSelectIconRight: true,
+        ...props.menuProps
+      }
+    })()
+    return (
+      <NextSelect
+        autoWidth={false}
+        {...props}
+        menuProps={menuProps}
+        ref={ref as any}
+      />
+    )
   }
-)as any
+) as any
 
 Select = HOC(Select) as any
 
