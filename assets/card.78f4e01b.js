7bcf4c488f5ab989ce9f55171319d2c98140e401
sirui.csr <sirui.csr@alibaba-inc.com>
2021-02-03 19:04:41 +0800

    Updates

---
+++ assets/card.78f4e01b.js
@@ -0,0 +1,53 @@
+var e=Object.assign;import{r as t,J as r,F as a,B as n}from"./index.3d842b9a.js";import{q as l}from"./styled-components.browser.esm.b64bc59a.js";var i=Object.freeze({__proto__:null,[Symbol.toStringTag]:"Module",default:e=>{const[a,n]=t.useState(!0);return t.createElement(r,{free:!0,contentHeight:"auto",style:{width:300}},t.createElement(r.CollapsableHead,{collapsed:a,onCollapsedChange:n},"项目标题"),a&&t.createElement(r.Content,null,"阿里云公共云控制台是一个包含了100+云产品的庞大产品集群，是一个结构复杂且庞大，目前仍在快速迭代更新的产品体系。我们通过大量实践、梳理和打磨，呈现出公共云控制台设计系统。将为阿里云公共云提供前沿创新、包容且智能的产品解决方案。提供轻松、可信赖的使用体验。呈现简洁有序、精致的视觉体验。"))}});const c={title:"Title",style:{width:300},subTitle:"Sub-title",extra:t.createElement(n,{text:!0,type:"primary"},"Link")};const o=l.div``;var m=Object.freeze({__proto__:null,[Symbol.toStringTag]:"Module",default:function(){const l=t.createElement(a,{direction:"row",spacing:20},t.createElement(r,{free:!0,style:{width:300}},t.createElement(r.Header,e({title:"Simple Card"},c)),t.createElement(r.Divider,null),t.createElement(r.Content,null,"Lorem ipsum dolor sit amet, est viderer iuvaret perfecto et. Ne petentium quaerendum nec, eos ex recteque mediocritatem, ex usu assum legendos temporibus. Ius feugiat pertinacia an, cu verterem praesent quo."),t.createElement(r.Divider,null),t.createElement(r.Actions,null,t.createElement(n,{type:"primary",key:"action1",text:!0},"Action 1"),t.createElement(n,{type:"primary",key:"action2",text:!0},"Action 2"))),t.createElement(r,{free:!0,style:{width:300}},t.createElement(r.Header,e({title:"Simple Card"},c)),t.createElement(r.Divider,{inset:!0}),t.createElement(r.Content,null,"Lorem ipsum dolor sit amet, est viderer iuvaret perfecto et. Ne petentium quaerendum nec, eos ex recteque mediocritatem, ex usu assum legendos temporibus. Ius feugiat pertinacia an, cu verterem praesent quo."),t.createElement(r.Divider,{inset:!0}),t.createElement(r.Actions,null,t.createElement(n,{type:"primary",key:"action1",text:!0},"Action 1"),t.createElement(n,{type:"primary",key:"action2",text:!0},"Action 2"))));return t.createElement(o,null,l)}});const u={subTitle:"SubTitle",extra:t.createElement(n,{text:!0,type:"primary"},"Link")};const s=l.div`
+  .card-placeholder {
+    height: 200px;
+  }
+`;var d=Object.freeze({__proto__:null,[Symbol.toStringTag]:"Module",default:function(){const a=t.createElement("div",null,t.createElement(r,{free:!0,style:{width:300}},t.createElement(r.Header,e({title:"Simple Card"},u)),t.createElement(r.Content,null,"Lorem ipsum dolor sit amet, est viderer iuvaret perfecto et. Ne petentium quaerendum nec, eos ex recteque mediocritatem, ex usu assum legendos temporibus. Ius feugiat pertinacia an, cu verterem praesent quo.")));return t.createElement(s,null,a)}});const p=l.div`
+  .media-card {
+    width: 300px;
+  }
+
+  .custom-card p {
+    color: #999;
+  }
+`;var E=Object.freeze({__proto__:null,[Symbol.toStringTag]:"Module",default:function(){const e=t.createElement(r,{className:"media-card",free:!0},t.createElement(r.Media,null,t.createElement("img",{src:"https://img.alicdn.com/tfs/TB1FNIOSFXXXXaWXXXXXXXXXXXX-260-188.png"})),t.createElement(r.Header,{title:"Title",subTitle:"Sub Title",extra:[t.createElement(n,{type:"primary",key:"action1",text:!0},"More")]}),t.createElement(r.Content,null,"Lorem ipsum dolor sit amet, est viderer iuvaret perfecto et. Ne petentium quaerendum nec, eos ex recteque mediocritatem, ex usu assum legendos temporibus. Ius feugiat pertinacia an, cu verterem praesent quo."),t.createElement(r.Actions,null,t.createElement(n,{type:"primary",key:"action1",text:!0},"Action 1"),t.createElement(n,{type:"primary",key:"action2",text:!0},"Action 2")));return t.createElement(p,null,e)}});const f=l.div`
+  .free-card {
+    display: inline-block;
+    width: 300px;
+    vertical-align: top;
+  }
+  .free-card + .free-card {
+    margin-left: 20px;
+  }
+
+  .free-card.custom {
+    display: flex;
+    justify-content: space-between;
+    width: 500px;
+    height: 200px;
+  }
+
+  .free-card.custom .next-card-media {
+    height: 100%;
+    width: 200px;
+    flex-shrink: 0;
+  }
+
+  .free-card-main {
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
+  }
+
+  .free-card-main .next-card-content-container {
+    flex: 1;
+  }
+
+  .free-card-main .next-card-actions {
+    text-align: right;
+  }
+`;var y=Object.freeze({__proto__:null,[Symbol.toStringTag]:"Module",default:function(){const e=t.createElement("div",null,t.createElement(r,{className:"free-card custom",free:!0},t.createElement(r.Media,{image:"https://img.alicdn.com/tfs/TB1FNIOSFXXXXaWXXXXXXXXXXXX-260-188.png"}),t.createElement("div",{className:"free-card-main"},t.createElement(r.Header,{title:"Title",subTitle:"Sub Title",extra:t.createElement(n,{type:"primary",text:!0},"Link")}),t.createElement(r.Content,null,"Lorem ipsum dolor sit amet, est viderer iuvaret perfecto et."),t.createElement(r.Actions,null,t.createElement(n,{type:"primary",key:"action1",text:!0},"Action 1"),t.createElement(n,{type:"primary",key:"action2",text:!0},"Action 2")))),t.createElement("br",null),t.createElement(r,{className:"free-card",free:!0},t.createElement(r.Media,{style:{height:140},image:"https://img.alicdn.com/tfs/TB1FNIOSFXXXXaWXXXXXXXXXXXX-260-188.png"}),t.createElement(r.Header,{title:"Title",subTitle:"Sub Title",extra:t.createElement(n,{type:"primary",text:!0},"Link")}),t.createElement(r.Divider,null),t.createElement(r.Content,null,"Lorem ipsum dolor sit amet, est viderer iuvaret perfecto et. Ne petentium quaerendum nec, eos ex recteque mediocritatem, ex usu assum legendos temporibus. Ius feugiat pertinacia an, cu verterem praesent quo."),t.createElement(r.Actions,null,t.createElement(n,{type:"primary",key:"action1",text:!0},"Action 1"),t.createElement(n,{type:"primary",key:"action2",text:!0},"Action 2"))));return t.createElement(f,null,e)}});var g=Object.freeze({__proto__:null,[Symbol.toStringTag]:"Module",default:e=>{const[a,n]=t.useState(!0);return t.createElement(r,{free:!0,contentHeight:"auto",style:{width:300}},t.createElement(r.Header,{title:"最新热点"}),t.createElement(r.Header,{title:"AI如何把招人的效率提高四成"}),t.createElement(r.Content,null,a?"阿里云公共云控制台是一个包含了100+云产品的庞大产品集群，是一个结构复杂且庞大，目前仍在快速迭代更新的产品体系。我们通过大量实践、梳理和打磨，呈现出公共云控制台设计系统。":"阿里云公共云控制台是一个包含了100+云产品的庞大产品集群，是一个结构复杂且庞大，目前仍在快速迭代更新的产品体系。我们通过大量实践、梳理和打磨，呈现出公共云控制台设计系统。将为阿里云公共云提供前沿创新、包容且智能的产品解决方案。提供轻松、可信赖的使用体验。呈现简洁有序、精致的视觉体验。"),t.createElement(r.CollapsableTail,{collapsed:a,onCollapsedChange:n},"展开更多"))}});const x={extra:t.createElement(r.DropDownActions,{actions:[{label:"Action1",onClick:()=>{console.log("Action1 clicked")}},{label:"Action2",onClick:()=>{console.log("Action2 clicked")}}]})};const X=l.div`
+  .card-placeholder {
+    height: 200px;
+  }
+`;var b=Object.freeze({__proto__:null,[Symbol.toStringTag]:"Module",default:function(){return t.createElement(X,null,t.createElement("div",null,t.createElement(r,{free:!0,style:{width:300}},t.createElement(r.Header,e({title:"Simple Card"},x)),t.createElement(r.Content,null,"Lorem ipsum dolor sit amet, est viderer iuvaret perfecto et. Ne petentium quaerendum nec, eos ex recteque mediocritatem, ex usu assum legendos temporibus. Ius feugiat pertinacia an, cu verterem praesent quo."))))}});const v={};v["_collapsable-head"]=i,v._divider=m,v._basic=d,v._media=E,v._free=y,v["_collapsable-tail"]=g,v["_header-actions"]=b;export default v;
