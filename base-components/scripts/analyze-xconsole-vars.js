cc3176296ad8b82e9576efcf366bf9a60f3b16ae
sirui.csr <sirui.csr@alibaba-inc.com>
2021-03-29 15:08:03 +0800

    add xconsole icon demo

--- base-components/scripts/analyze-xconsole-vars.js
+++ base-components/scripts/analyze-xconsole-vars.js
@@ -18,18 +18,48 @@ const xconsoleVarNames = Object.keys(xconsoleVar)
 
 const collapse = []
 
-const differenceBefore = difference(windVarNames, xconsoleVarNames)
-if (differenceBefore[0].length > 0) {
-  throw new Error('unexpect')
-}
+const windIcons = windVarNames
+  .filter((i) => i.startsWith('icon-content-'))
+  .map((i) => {
+    return i.replace(/^icon-content-/, '')
+  })
+  .sort()
 
-const differenceAfter = difference(unify(windVarNames), unify(xconsoleVarNames))
+const xconsoleIcons = xconsoleVarNames
+  .filter((i) => i.startsWith('icon-content-'))
+  .map((i) => {
+    return i.replace(/^icon-content-/, '')
+  })
+  .sort()
 
-if (differenceAfter[0].length > 0) {
-  throw new Error('unexpect2')
-}
+fs.writeFileSync(
+  path.join(__dirname, '../src/icon/wind-icon-types.js'),
+  `export default ${JSON.stringify(windIcons, null, 2)}`
+)
 
-console.log(collapse)
+fs.writeFileSync(
+  path.join(__dirname, '../src/icon/xconsole-icon-types.js'),
+  `export default ${JSON.stringify(xconsoleIcons, null, 2)}`
+)
+
+// const differenceBefore = difference(windVarNames, xconsoleVarNames)
+// if (differenceBefore[0].length > 0) {
+//   throw new Error('unexpect')
+// }
+
+// const newIcons = differenceBefore[1].filter((i) => i.startsWith('icon-content-')).map((i) => {
+//   return i.replace(/^icon-content-/, '')
+// })
+
+// fs.writeFileSync('out.txt', newIcons.join('\n'))
+
+// const differenceAfter = difference(unify(windVarNames), unify(xconsoleVarNames))
+
+// if (differenceAfter[0].length > 0) {
+//   throw new Error('unexpect2')
+// }
+
+// console.log(collapse)
 
 function extractVars(code) {
   const vars = code.matchAll(/--(.*?):\s*(.*?);/g)
@@ -50,6 +80,7 @@ function difference(arr1, arr2) {
   return [d1, d2]
 }
 
+// 有些var名字使用_作为分隔符
 function unify(strArr) {
   const set = new Set(strArr)
   return strArr.map((str) => {
