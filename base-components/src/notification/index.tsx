a4cd3ccc30d5fb038c12879c29f60eec39907aad
zuoqi705 <1398772969@qq.com>
2021-04-28 20:35:57 +0800

    style(Notification): adjust default offset

--- base-components/src/notification/index.tsx
+++ base-components/src/notification/index.tsx
@@ -1,6 +1,17 @@
-
   import { Notification as NextNotification } from '@alifd/next'
 
+  // 调整默认 offset 值
+  const theme = window.getComputedStyle?.(window.document.body).getPropertyValue('--alicloudfe-components-theme').trim();
+  if (
+    theme === 'yunxiao' ||
+    theme === 'yunxiao-dark' ||
+    theme === 'hybridcloud' ||
+    theme === 'hybridcloud-dark'
+  ) {
+    NextNotification.config({
+      offset: [24, 16]
+    })
+  }
   export default NextNotification;
     
   
