dd295afe1926d99c3170a0a688bc1274af15ee88
zuoqi705 <1398772969@qq.com>
2021-04-29 13:57:31 +0800

    feat: 1.1.71

--- base-components/src/notification/index.tsx
+++ base-components/src/notification/index.tsx
@@ -1,17 +1,3 @@
-  import { Notification as NextNotification } from '@alifd/next'
+import { Notification as NextNotification } from '@alifd/next'
 
-  // 调整默认 offset 值
-  const theme = window.getComputedStyle?.(window.document.body).getPropertyValue('--alicloudfe-components-theme').trim();
-  if (
-    theme === 'yunxiao' ||
-    theme === 'yunxiao-dark' ||
-    theme === 'hybridcloud' ||
-    theme === 'hybridcloud-dark'
-  ) {
-    NextNotification.config({
-      offset: [24, 16]
-    })
-  }
-  export default NextNotification;
-    
-  
+export default NextNotification
