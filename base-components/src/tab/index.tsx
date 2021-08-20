c2fbac2c4e04434d69f0c77b0e4fd250cf3cc114
wwtplus <2509625275@qq.com>
2021-08-20 16:17:58 +0800

    fix: remove tab animation

--- base-components/src/tab/index.tsx
+++ base-components/src/tab/index.tsx
@@ -1,4 +1,41 @@
 import { Tab as NextTab } from '@alifd/next'
+import React, { Children } from 'react'
 import { withThemeClass } from '../utils/withThemeClass'
+import hoistNonReactStatics from 'hoist-non-react-statics'
+import cls from 'classnames'
 
-export default withThemeClass(NextTab)
+import { useCssVar } from '../utils/useCssVar'
+
+// export default withThemeClass(NextTab)
+
+type NextTabProps = React.ComponentProps<typeof NextTab>
+
+const Tab: typeof NextTab = withThemeClass(
+  React.forwardRef((props: NextTabProps, ref) => {
+    const { children } = props;
+    console.log(props);
+    
+    const theme = useCssVar('--alicloudfe-components-theme').trim()
+    console.log(theme);
+    
+    if (theme === 'hybridcloud' || theme === "hybridcloud-dark") {
+      return (
+        <NextTab animation={false} {...props} ref={ref as any}>
+          {children}
+        </NextTab>
+      )
+    }
+    return (
+      <NextTab {...props} ref={ref as any}>
+        {children}
+      </NextTab>
+    )
+  })
+) as any
+
+hoistNonReactStatics(Tab, NextTab)
+
+// @ts-ignore
+Tab.displayName = NextTab.displayName
+
+export default Tab
