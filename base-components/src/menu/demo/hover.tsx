c4393b8dbb9926be22834c6474edef290806af4d
wwtplus <2509625275@qq.com>
2021-11-08 16:25:17 +0800

    feat(menu): long text adaptation

--- base-components/src/menu/demo/hover.tsx
+++ base-components/src/menu/demo/hover.tsx
@@ -17,7 +17,7 @@ export default function DemoComponent() {
       <Item key="2">Option 2</Item>
       <Item key="3">Option 3</Item>
       <Divider key="divider" />
-      <SubMenu key="sub-1" label="Popup menu 1">
+      <SubMenu key="sub-1" label="Popup menu 1 Popup menu 1 Popup menu 1 Popup menu 1 Popup menu 1 Popup menu 1">
         <Item key="popup-1-1">Popup option 1</Item>
         <Item key="popup-1-2">Popup option 2</Item>
       </SubMenu>
