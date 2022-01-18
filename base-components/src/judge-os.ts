b2bc5979854defcae282db698261a8d373bbf81e
zuoqi705 <1398772969@qq.com>
2022-01-18 12:37:20 +0800

    feat: support ssr

--- base-components/src/judge-os.ts
+++ base-components/src/judge-os.ts
@@ -1,22 +1,29 @@
 // 判断操作系统，如果是 windows 的话在 body 上加一个类名用来区分
+const isBrowser = (): boolean =>
+  typeof window !== 'undefined' &&
+  typeof window.document !== 'undefined' &&
+  typeof window.document.createElement !== 'undefined'
+
 const judgeOs = () => {
-  if (window && window.navigator) {
-    const agent = navigator?.userAgent?.toLowerCase()
-    if (
-      agent.indexOf('win32') >= 0 ||
-      agent.indexOf('wow32') >= 0 ||
-      agent.indexOf('win64') >= 0 ||
-      agent.indexOf('wow64') >= 0
-    ) {
-      if (window.document && window.document.body) {
-        if (!document.body?.classList?.contains('isWindows')) {
-          document.body.classList.add('isWindows')
+  if (isBrowser()) {
+    if (window && window.navigator) {
+      const agent = navigator?.userAgent?.toLowerCase()
+      if (
+        agent.indexOf('win32') >= 0 ||
+        agent.indexOf('wow32') >= 0 ||
+        agent.indexOf('win64') >= 0 ||
+        agent.indexOf('wow64') >= 0
+      ) {
+        if (window.document && window.document.body) {
+          if (!document.body?.classList?.contains('isWindows')) {
+            document.body.classList.add('isWindows')
+          }
         }
-      }
-    } else if (/macintosh|mac os x/i.test(navigator?.userAgent)) {
-      if (window.document && window.document.body) {
-        if (!document.body?.classList?.contains('isMac')) {
-          document.body.classList.add('isMac')
+      } else if (/macintosh|mac os x/i.test(navigator?.userAgent)) {
+        if (window.document && window.document.body) {
+          if (!document.body?.classList?.contains('isMac')) {
+            document.body.classList.add('isMac')
+          }
         }
       }
     }
