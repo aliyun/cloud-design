39474634630b6457232d84385ae4c60012d7a70a
龙炽 <wangweitao.wwt@alibaba-inc.com>
2022-08-26 14:33:15 +0800

    feat: 瘦身css var

--- base-components/scripts/build-css.js
+++ base-components/scripts/build-css.js
@@ -5,24 +5,35 @@ const postcss = require('postcss')
 const themes = require('./theme-data')
 const log = console.log
 const chalk = require('chalk')
+const filterCssVar = require('./filter-css-var')
+
 
 const convertCharStr2CSS = ch => {
-  let code = ch.charCodeAt(0).toString(16);
+  let code = ch.charCodeAt(0).toString(16)
   while (code.length < 4) {
-    code = `0${code}`;
+    code = `0${code}`
   }
-  return `\\${code}`.toUpperCase();
-};
+  return `\\${code}`.toUpperCase()
+}
 
 const transferUniCodeCss = (source, theme) => {
   if (theme !== 'wind' &&
     !theme.startsWith('xconsole')) {
     return source.replace(/content:\s*(?:'|")([\u0080-\uffff])(?:'|")/g, (str, $1) => {
-      return `content: "${convertCharStr2CSS($1)}"`;
+      return `content: "${convertCharStr2CSS($1)}"`
     })
       .replace(/content:\s*var\((--[\w-]+\,\s*)(?:'|")([\u0080-\uffff])(?:'|")\)/g, (str, $1, $2) => {
-        return `content: var(${$1}"${convertCharStr2CSS($2)}")`;
+        return `content: var(${$1}"${convertCharStr2CSS($2)}")`
+      })
+      .replace(/(?!var\()--icon-content.*:\s*['"](.*?)['"]/g, (str, match) => {
+        if (match) {
+          return str.replace(/(?<=").*?(?=")/g, (char) => {
+            return `${convertCharStr2CSS(char)}`;
+          });
+        }
+        return str
       });
+
   } else {
     return source
   }
@@ -39,7 +50,7 @@ themes.forEach(async (theme) => {
     file: path.join(__dirname, `../src/theme/${themeName}/index.scss`)
   })
   fs.ensureDirSync(path.join(__dirname, '../dist'))
-  fs.writeFileSync(path.join(__dirname, `../dist/${themeName}.css`), transferUniCodeCss(result.css.toString(), themeName))
+  fs.writeFileSync(path.join(__dirname, `../dist/${themeName}.css`), filterCssVar(transferUniCodeCss(result.css.toString(), themeName)))
 
   // 生成不带css var定义的css
   log(`generate ${themeName}-no-var.css...`)
@@ -58,9 +69,23 @@ themes.forEach(async (theme) => {
     })
     fs.writeFileSync(
       path.join(__dirname, `../dist/${themeName}-no-reset.css`),
-      transferUniCodeCss(noVarResult.css.toString(), themeName)
+      filterCssVar(transferUniCodeCss(noVarResult.css.toString(), themeName))
     )
   }
+
+
+    // 生成每个主题的css-var定义文件，用于动态切换
+    const cssVarDefinitionPath = path.join(
+      __dirname,
+      `../pages/theme-vars/${themeName}.scss`
+    )
+    const cssVarDefinitionResult = sass.renderSync({ file: cssVarDefinitionPath })
+     // 去掉注释
+     const varCss = cssVarDefinitionResult.css
+     .toString()
+     .replace(/\/\*[.\t\n\r\S\s]*?\*\//g, '')
+    log(`generate ${themeName}-var.css...`)
+    fs.writeFileSync(path.join(__dirname, `../dist/${themeName}-var.css`), filterCssVar(transferUniCodeCss(varCss, themeName)))
 
   // 去掉注释
   const noVarCss = noVarResult.css
@@ -69,7 +94,7 @@ themes.forEach(async (theme) => {
   fs.ensureDirSync(path.join(__dirname, '../dist'))
   fs.writeFileSync(
     path.join(__dirname, `../dist/${themeName}-no-var.css`),
-    transferUniCodeCss(noVarCss.toString(), themeName)
+    filterCssVar(transferUniCodeCss(noVarCss, themeName), varCss)
   )
 
   // 生成压缩过的 css
@@ -96,20 +121,8 @@ themes.forEach(async (theme) => {
     path.join(__dirname, `../dist/${themeName}.min.css`),
     minifyCssResult
   )
-
-  // 生成每个主题的css-var定义文件，用于动态切换
-  const cssVarDefinitionPath = path.join(
-    __dirname,
-    `../pages/theme-vars/${themeName}.scss`
-  )
-  const cssVarDefinitionResult = sass.renderSync({ file: cssVarDefinitionPath })
-  // 去掉注释
-  const varCss = cssVarDefinitionResult.css
-    .toString()
-    .replace(/\/\*[.\t\n\r\S\s]*?\*\//g, '')
-  log(`generate ${themeName}-var.css...`)
-  fs.writeFileSync(path.join(__dirname, `../dist/${themeName}-var.css`), transferUniCodeCss(varCss, themeName))
 })
+
 
 // 修改css选择器的postcss插件
 // function changePrefix(transform) {
@@ -124,3 +137,4 @@ themes.forEach(async (theme) => {
 //     })
 //   }
 // }
+
